ab95a5b883225dea991fd3347e0c99ae48030c65
Jean Markis <53277311+Maarkis@users.noreply.github.com>
2020-12-18 04:02:10 -0300

    Comentando Context Factory

--- AplicacoesDistribuidasAPI.Data/Context/ContextFactory.cs
+++ AplicacoesDistribuidasAPI.Data/Context/ContextFactory.cs
@@ -4,26 +4,26 @@ using Microsoft.Extensions.Configuration;
 
 namespace AplicacoesDistribuidasAPI.Data.Context
 {
-    public class ContextFactory : IDesignTimeDbContextFactory<DataBaseContext>
-    {
-        //private readonly IConfiguration _configuration;
+    //public class ContextFactory : IDesignTimeDbContextFactory<DataBaseContext>
+    //{
+    //    //private readonly IConfiguration _configuration;
 
-        //public ContextFactory(IConfiguration configuration)
-        //{
-        //    _configuration = configuration;
-        //}
+    //    //public ContextFactory(IConfiguration configuration)
+    //    //{
+    //    //    _configuration = configuration;
+    //    //}
 
-        //public ContextFactory(IConfiguration configuration)
-        //{
-        //    _configuration = configuration;
-        //}
-        public DataBaseContext CreateDbContext(string[] args)
-        {
-            string connectString = "Server=localhost;Port=3306;Database=aplicacoesDistribuidas;Uid=root;Pwd=123456";
-            DbContextOptionsBuilder<DataBaseContext> optionBuilder = new DbContextOptionsBuilder<DataBaseContext>();
-            optionBuilder.UseMySql(connectString);
-            return new DataBaseContext(optionBuilder.Options);
+    //    //public ContextFactory(IConfiguration configuration)
+    //    //{
+    //    //    _configuration = configuration;
+    //    //}
+    //    //public DataBaseContext CreateDbContext(string[] args)
+    //    //{
+    //    //    string connectString = "Server=localhost;Port=3306;Database=aplicacoesDistribuidas;Uid=root;Pwd=123456";
+    //    //    DbContextOptionsBuilder<DataBaseContext> optionBuilder = new DbContextOptionsBuilder<DataBaseContext>();
+    //    //    optionBuilder.UseMySql(connectString);
+    //    //    return new DataBaseContext(optionBuilder.Options);
 
-        }
-    }
+    //    //}
+    //}
 }
